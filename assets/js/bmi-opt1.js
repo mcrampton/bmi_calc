f6c6fe9185754d6a0e3b39f42218e88d506a2529
BigMac <BigMac@bigmac.home>
2017-01-29 19:45:05 +0000

    Working calculation metric imperial

--- assets/js/bmi-opt1.js
+++ assets/js/bmi-opt1.js
@@ -1,47 +1,51 @@
-function $(){
-var elements=new Array();
-for (var i=0;i<arguments.length;i++){
+function $() {
+var elements = new Array();
+for (var i=0;i<arguments.length;i++) {
 var element=arguments[i];
-if(typeof element=='string')element=document.getElementById(element);
-if(arguments.length==1)return element;
+if(typeof element==='string') element=document.getElementById(element);
+if(arguments.length===1) return element;
 elements.push(element);
 }
-return elements;
 }
-function msystem(){
-if($('msw').value=='metric'){
-$('thw').innerHTML=' (Cms)';
-}else{
-$('thw').innerHTML=' (inches)';
-};
-if($('msm').value=='metric'){
-$('thm').innerHTML=' (Cms)';
-$('twm').innerHTML=' (Kgs)';
-}else{
-$('thm').innerHTML=' (inches)';
-$('twm').innerHTML=' (lbs)';
-};
-if($('msf').value=='metric'){$('thf').innerHTML=' (Cms)';
-$('tneck').innerHTML=' (Cms)';
-$('twaist').innerHTML=' (Cms)';
-$('thips').innerHTML=' (Cms)';
-}else{
-$('thf').innerHTML=' (inches)';
-$('tneck').innerHTML=' (inches)';
-$('twaist').innerHTML=' (inches)';
-$('thips').innerHTML=' (inches)';
-}
-}
+
+
+function msystem() {
+	
+	if (document.getElementById("msm1").checked === true) {
+        $('thm').innerHTML = ' (Cms)';
+        $('twm').innerHTML = ' (Kgs)';
+	    }
+	    else if (document.getElementById("msm2").checked === true) {
+	        $('thm').innerHTML = ' (inches)';
+	        $('twm').innerHTML = ' (lbs)';
+	    }
+	    else {
+	        // DO NOTHING
+	        }
+	    }
+	
 function bmass (){
-var ms=$('msm').value;var height=$('hm').value;
-var weight=$('wm').value;if(height==null || height.length==0 || weight==null || weight.length==0 ){
+	var measure= document.querySelector('input[name="msm"]:checked').value;
+	
+	//arguments[i];
+	//if (document.getElementById("msm1").checked === true) {
+	//	  measure-type = document.getElementById('msm1').value;
+	//  };
+	//if (document.getElementById("msm1").checked === true) {
+	//	  measure-type = document.getElementById('msm2').value;
+	//  };
+
+var ms= measure,
+	height=$('hm').value,
+	weight=$('wm').value;
+if(height===null || height.length===0 || weight===null || weight.length===0 ){
 $('bmi').value="Pl. enter data.";
 }else{
 $('bmi').value="";
 }
-if(ms=='metric'&&height>0){
-$('bmi').value=Math.round(weight/(height*height/10000)*100)/100+" kg/m2 " 
-}else if(ms=='us'&&height>0){
-$('bmi').value=Math.round(703*weight/(height*height)*100)/100+" kg/m2 " 
+if(ms==='metric'&&height>0){
+	$('bmi').value=Math.round(weight/(height*height/10000)*100)/100+" kg/m2 " ;
+}else if(ms==='us'&&height>0){
+	$('bmi').value=Math.round(703*weight/(height*height)*100)/100+" lbs/m2 " ;
 }
 }
--- assets/js/bmi__calc.js
+++ assets/js/bmi__calc.js
@@ -1,6 +1,62 @@
+function formatDecimal(num, numPlaces){
+	var snum = new String(parseFloat(num))
+	var i=z=0; 
+	var sout=ch="" 
+	while(i<snum.length && ch != '.' ){
+		ch = snum.charAt(i++)
+		sout+=ch
+	}
+	while(i<snum.length && z++<numPlaces){
+		ch = snum.charAt(i++)
+		sout+=ch
+	}
+	if(snum.indexOf('.')==-1)sout+='.';
+	while(z++<numPlaces)sout+='0';
+	return sout
+}
+function setfocus(f){
+	f.focus();
+	f.select();
+}
+function makepos(n){
+	return (n<0)? -n: n;
+}
+
+var strMess= new Array(
+	'Underweight',
+	'\'Normal\' weight (Grade 0)',
+	'Overweight\/\'plump\' (Grade I)',
+	'Moderately obese (Grade II)',
+	'Severely obese (Grade III)'
+);
 function calcbmi(){
-	var f= document.frmBMI,
-	 kg=f.txtKG.value,
-	 m=f.txtMETRE.value,
-	 b=idx=0;
+	var f= document.frmBMI
+	var kg=f.txtKG.value
+	var m=f.txtMETRE.value;
+	var b=idx=0;
+	if(!makepos(parseFloat(kg))>0 ){
+		alert('Please enter a valid weight')
+		setfocus(f.txtKG)
+	}else if(!makepos(parseFloat(m))>0){
+		alert('Please enter a valid height')
+		setfocus(f.txtMETRE)
+	}else{
+		if(!f.cmbWeight.selectedIndex==0)kg/=2.2046226
+		if(!f.cmbHeight.selectedIndex==0)m*=0.0254
+		b =  kg / (m * m)
+		if(b<20)idx=0
+		else if(b<25)idx=1
+		else if(b<30)idx=2
+		else if(b<40)idx=3
+		else idx=4
+		f.txtBMI.value=formatDecimal(b,2)
+		f.txtmessage.value=strMess[idx]
+	}
+}
+function clearbmi(){
+	var f= document.frmBMI
+	f.txtKG.value=''
+	f.txtMETRE.value=''
+	f.txtBMI.value=''
+	f.txtmessage.value=''
 }
