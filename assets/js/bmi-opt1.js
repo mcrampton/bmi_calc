b3a82b9a949d4fdabf4d987dd81ccd750963c9fa
Blockhead61 <matcrampton@gmail.com>
2017-01-30 16:44:23 +0000

    Update bmi-opt1.js

--- assets/js/bmi-opt1.js
+++ assets/js/bmi-opt1.js
@@ -36,20 +36,22 @@ $('bmi').textContent="Pl. enter data.";
 $('bmi').textContent="";
 }
 var you = '';
-
+if((ms==='metric'&&height>0) || (ms==='us'&&height>0)) {
+    yourResult.innerHTML = 'You BMI is:';
+}
 if(ms==='metric'&&height>0){
 	$('bmi').textContent=Math.round(weight/(height*height/10000)*100)/100 ;  
 }else if(ms==='us'&&height>0){
 	$('bmi').textContent=Math.round(703*weight/(height*height)*100)/100;
 }
 if ($('bmi').textContent < 18.5) {
-	you = 'Underweight';
+	you = 'You are below the recommended weight for your height and should not be attempting to lose weight. A healthy BMI range is between 18.5-25. We recommend that you consult your GP.</p>';
 } else if ($('bmi').textContent > 18.5 && ($('bmi').textContent < 24.9)) {
-	you = 'Normal';
+	you = 'You are a normal weight range for your height. However it is still important to eat healthily.';
 } else if ($('bmi').textContent > 24.9 && ($('bmi').textContent < 29.9)) {
-	you = 'Overweight';
+	you = 'You are within the overweight range and your health will benefit if you lost a little weight. You certaintly don\'t want to put on any weight. Look at the BMI chart to see your ideal weight range for your height.';
 } else {
-	you = 'Obese';
+	you = 'You are classified as obese and your health will greatly benefit if you were able to lose weight. Being obese means that you are at increased risk of diet related diseases such as diabetes and heart disease. Look at the BMI chart to see your ideal weight range for your height. We recommend that you visit your GP to discuss losing weight safely through getting more active and a health balanced diet.';
 }
 youAre.innerHTML = you;
 
